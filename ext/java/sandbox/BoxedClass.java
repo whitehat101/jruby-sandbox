d4de35baa0d0cd3dec7d4a48479b535a1ec66d91
Jeremy Ebler <jebler@gmail.com>
2013-12-03 20:24:26 -0800

    Prohibit access to eval on ref’d objects (resolve pending spec)

--- ext/java/sandbox/BoxedClass.java
+++ ext/java/sandbox/BoxedClass.java
@@ -2,6 +2,7 @@ package sandbox;
 
 import org.jruby.anno.JRubyClass;
 import org.jruby.anno.JRubyMethod;
+import org.jruby.exceptions.RaiseException;
 import org.jruby.runtime.builtin.IRubyObject;
 import org.jruby.runtime.Block;
 
@@ -12,6 +13,10 @@ public class BoxedClass {
     IRubyObject[] args2 = new IRubyObject[args.length - 1];
     System.arraycopy(args, 1, args2, 0, args2.length);
     String name = args[0].toString();
+
+    // Never allow access to eval on linked objects
+    if(name == "eval")
+      throw new RaiseException(recv.getRuntime(), recv.getRuntime().getSecurityError(), "eval not allowed on " + recv.asString().asJavaString(), false);
 
     SandboxFull box = (SandboxFull) SandboxFull.getLinkedBox(recv);
     return box.runMethod(recv, name, args2, block);
